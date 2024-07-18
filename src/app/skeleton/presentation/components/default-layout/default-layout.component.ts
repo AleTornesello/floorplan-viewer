4bedd0cfbcb36930ac04d76a0203616b4d2ce64a
AleTornesello <alessandro.tornesello99@gmail.com>
2024-07-18 19:19:07 +0200

    feat: replace sidebar with bottom navbar in mobile view

--- src/app/skeleton/presentation/components/default-layout/default-layout.component.ts
+++ src/app/skeleton/presentation/components/default-layout/default-layout.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopbarComponent } from '../topbar/topbar.component';
+import { BottomNavbarComponent } from '../bottom-navbar/bottom-navbar.component';
 
 @Component({
   selector: 'app-default-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TopbarComponent],
+  imports: [CommonModule, RouterOutlet, TopbarComponent, BottomNavbarComponent],
   templateUrl: './default-layout.component.html',
   styleUrl: './default-layout.component.scss',
 })
