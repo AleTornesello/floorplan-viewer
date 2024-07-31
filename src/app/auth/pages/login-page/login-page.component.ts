520bd085f2ac1e2e7f451ee4762ed38791a972c2
AleTornesello <alessandro.tornesello99@gmail.com>
2024-07-31 19:50:10 +0200

    feat: add login flow

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,12 +1,18 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators,} from '@angular/forms';
-import {TranslocoPipe} from '@jsverse/transloco';
+import {TranslocoPipe, TranslocoService} from '@jsverse/transloco';
 import {CardModule} from 'primeng/card';
 import {ButtonComponent} from '../../../shared/components/button/button.component';
-import {RouterModule} from '@angular/router';
+import {Router, RouterModule} from '@angular/router';
 import {MsjRoute} from '../../../app.routes';
 import {InputPasswordComponent} from "../../../shared/components/inputs/input-password/input-password.component";
 import {InputTextComponent} from "../../../shared/components/inputs/input-text/input-text.component";
+import {SupabaseAuthService} from "../../services/supabase-auth.service";
+import {AuthTokenResponsePassword} from "@supabase/supabase-js";
+import {
+  ResultMessageDialogComponent
+} from "../../../shared/components/dialog/result-message-dialog/result-message-dialog.component";
+import {StringManipulationService} from "../../../shared/services/string-manipulation.service";
 
 @Component({
   selector: 'app-login-page',
@@ -19,14 +25,22 @@ import {InputTextComponent} from "../../../shared/components/inputs/input-text/i
     InputPasswordComponent,
     ButtonComponent,
     RouterModule,
+    ResultMessageDialogComponent,
   ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
 })
 export class LoginPageComponent {
+  @ViewChild('loginResultDialog') loginResultDialog!: ResultMessageDialogComponent;
   public form: FormGroup;
 
-  constructor(private _fb: FormBuilder) {
+  constructor(
+    private _fb: FormBuilder,
+    private _supabaseAuthService: SupabaseAuthService,
+    private _translationService: TranslocoService,
+    private _router: Router,
+    private _stringManipulationService: StringManipulationService
+  ) {
     this.form = this._buildForm();
   }
 
@@ -37,13 +51,45 @@ export class LoginPageComponent {
     });
   }
 
-  public onFormSubmit() {
+  public async onFormSubmit() {
     if (this.form.invalid) {
       this.form.markAllAsTouched();
       return;
     }
 
-    // TODO: Implement login
+    let loginResponse: AuthTokenResponsePassword | null = null;
+
+    try {
+      const email = this.form.get('email')!.value;
+      const password = this.form.get('password')!.value;
+      loginResponse = await this._supabaseAuthService.signIn(email, password);
+    } catch (error) {
+      this.loginResultDialog.show("error", this._translationService.translate('auth.loginKo'));
+      return;
+    }
+
+    if (!loginResponse) {
+      this.loginResultDialog.show("error", this._translationService.translate('auth.loginKo'));
+      return;
+    }
+
+    if (loginResponse.error) {
+
+      const errorMessage = loginResponse.error.code
+        ? this._translationService.translate(`auth.loginErrors.${this._stringManipulationService.snakeToCamel(loginResponse.error.code as string)}`)
+        : loginResponse.error.message;
+      this.loginResultDialog.show("error", errorMessage);
+      return;
+    }
+
+    const session = await this._supabaseAuthService.session;
+
+    if (!session) {
+      this.loginResultDialog.show("error", this._translationService.translate('auth.loginKo'));
+      return;
+    }
+
+    this._router.navigate([]);
   }
 
   public get registrationPageLink() {
--- src/app/auth/services/supabase-auth.service.ts
+++ src/app/auth/services/supabase-auth.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {AuthChangeEvent, AuthSession, createClient, Session, SupabaseClient,} from '@supabase/supabase-js';
+import {AuthChangeEvent, createClient, Session, SupabaseClient,} from '@supabase/supabase-js';
 import {environment} from '../../../environments/environment';
 
 @Injectable({
@@ -9,20 +9,14 @@ export class SupabaseAuthService {
   private _supabase: SupabaseClient;
 
   constructor() {
-    this._session = null;
     this._supabase = createClient(
       environment.supabase.url,
       environment.supabase.key
     );
   }
 
-  private _session: AuthSession | null;
-
   public get session() {
-    this._supabase.auth.getSession().then(({data}) => {
-      this._session = data.session;
-    });
-    return this._session;
+    return this._supabase.auth.getSession();
   }
 
   public authChanges(
@@ -46,6 +40,6 @@ export class SupabaseAuthService {
           emailRedirectTo
         }
         : undefined
-    });
+    })
   }
 }
